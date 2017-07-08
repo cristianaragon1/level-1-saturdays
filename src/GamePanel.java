e42343a22e4a880f7e0177e3b10c3b8c856cd191
Cristian Aragon <league@WTS-IM-17.attlocal.net>
2017-07-08 14:30:34 -0700

    resume on step 8

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -7,43 +9,117 @@ import java.awt.event.KeyListener;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class GamePanel extends JPanel implements ActionListener, KeyListener{
-	Timer gametime = new Timer(1000/60,this);
-	@Override
+public class GamePanel extends JPanel implements ActionListener, KeyListener {
+	Timer gametime = new Timer(1000 / 60, this);
+	Font titleFont;
+	Font titleFontTwo;
+	Font titleFontEnd;
+	Font titleFontEndTwo;
+	final int GAME_STATE = 0;
+	final int MENU_STATE = 1;
+	final int END_STATE = 2;
+	int currentState = MENU_STATE;
+	// GameObject gameob = new GameObject();
+
+	public GamePanel() {
+		titleFont = new Font("Arial", Font.PLAIN, 48);
+		titleFontTwo = new Font("Arial", Font.PLAIN, 28);
+		titleFontEnd = new Font("Arial", Font.PLAIN, 48);
+		titleFontEndTwo = new Font("Arial", Font.PLAIN, 28);
+	}
+
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		repaint();
+		if (currentState == MENU_STATE) {
+			updateMenuState();
+		} else if (currentState == GAME_STATE) {
+			updateGameState();
+		} else if (currentState == END_STATE) {
+			updateEndState();
+		}
+
+		// gameob.update();
 	}
 
+	public void startGame() {
+		gametime.start();
+	}
 
-public void startGame(){
-	gametime.start();
-}
-public void paintComponent(Graphics g){
-	g.fillRect(10, 10, 100, 100);
+	public void paintComponent(Graphics g) {
+		// gameob.draw(g);
+		if (currentState == MENU_STATE) {
+			drawMenuState(g);
+		} else if (currentState == GAME_STATE) {
+			drawGameState(g);
+		} else if (currentState == END_STATE) {
+			drawEndState(g);
+		}
 
-}
+	}
 
+	public void updateMenuState() {
 
-@Override
-public void keyTyped(KeyEvent e) {
-	// TODO Auto-generated method stub
-	System.out.println("keys");
-}
+	}
 
+	public void updateGameState() {
 
-@Override
-public void keyPressed(KeyEvent e) {
-	// TODO Auto-generated method stub
-	System.out.println("key p");
-}
+	}
 
+	public void updateEndState() {
 
-@Override
-public void keyReleased(KeyEvent e) {
-	// TODO Auto-generated method stub
-	System.out.println("key r");
-}
+	}
 
+	public void drawMenuState(Graphics g) {
+		g.setColor(Color.BLUE.darker());
+		g.fillRect(0, 0, 600, 900);
+		g.setColor(Color.yellow);
+		g.setFont(titleFont);
+		g.drawString("League Invaders", 60, 200);
+		g.setColor(Color.yellow);
+		g.setFont(titleFontTwo);
+		g.drawString("Press ENTER to start", 100, 300);
+
+	}
+
+	public void drawGameState(Graphics g) {
+		g.setColor(Color.black);
+		g.fillRect(0, 0, 600, 900);
+	}
+
+	public void drawEndState(Graphics g) {
+		g.setColor(Color.RED.darker());
+		g.fillRect(0, 0, 600, 900);
+		g.setColor(Color.black);
+		g.setFont(titleFontEnd);
+		g.drawString("GAME OVER", 100, 200);
+		g.setColor(Color.black);
+		g.setFont(titleFontEndTwo);
+		g.drawString("Press BACKSPACE to restart", 60, 300);
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("keys");
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("key p");
+		if (e.getKeyCode() == 10) {
+			currentState += 1;
+			if (currentState > END_STATE) {
+				currentState = MENU_STATE;
+			}
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("key r");
+	}
 
 }
